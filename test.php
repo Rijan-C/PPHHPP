d7777833252ebef156ff97106b4add42f96d03d1
Rijan Chudal <chudalrijan@gmail.com>
2025-09-04 07:44:10 +0545

    your message

--- test.php
+++ test.php
@@ -1,6 +1,16 @@
 <?php
+
+   echo "<h1> Arithmetic operators</h1>";
   $x = 10;
   $y = 20;
-  $z = $x + $y;
-  echo "The sum of $x and $y is $z.";
+  
+
+  echo "The sum of $x and $y is".($x + $y)."<br>";
+
+  echo "The difference of $x and $y is" .($x - $y)."<br>";
+
+  echo "The product of $x and $y is" .($x * $y)."<br>";
+
+  echo "The division of $x and $y is" .($x / $y)."<br>";
+
 ?>
